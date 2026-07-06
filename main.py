ea1d697df55df8c0f327a6fd96e9cbae4cf6b6b1
maload101 <marcellobentes@sapo.pt>
2026-07-06 16:11:04 -0300

    make script interactive

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello, AI Freelancer Journey!")
+user_name = input("What is your name? ")
+user_goal = input("What is your main goal with programming? ")
+
+print()
+print(f"Hello, {user_name}!")
+print(f"Your current goal is: {user_goal}")
+print("Let's build useful projects and turn them into real opportunities.")
