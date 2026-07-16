ef21ed22646351cf78265b173e58b34bfa44b358
maload101 <marcellobentes@sapo.pt>
2026-07-16 14:52:16 -0300

    Practice loading profile from JSON

--- json_practice.py
+++ json_practice.py
@@ -9,3 +9,14 @@ with open("profile.json", "w") as file:
     json.dump(user_profile, file)
 
 print("Profile saved as JSON.")
+
+with open("profile.json", "r") as file:
+    loaded_profile = json.load(file)
+
+print()
+print("Loaded profile:")
+print(loaded_profile)
+
+print()
+print(f"Name: {loaded_profile['name']}")
+print(f"Current goal: {loaded_profile['current_goal']}")
